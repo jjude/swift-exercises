57a9aa13754e30053c64cddccc84be64570858a3
Joseph Jude <joseph@jjude.com>
2015-12-19 07:30:24 +0530

    challenge-001

---
+++ 001-divisible-by-7-not-5.swift
@@ -0,0 +1,28 @@
+/*
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+Uses:
+ - declaring int empty array
+ - range
+ - modulo division
+ - append to array
+ - convert int to string
+ - map
+ - join elements of array with a separator
+*/
+
+var output: [Int] = []
+for i in 2000...3200{
+  if (i % 7 == 0) && (i % 5 != 0){
+    output.append(i)
+  }
+}
+
+var outInString = output.map(){ String($0) }
+print(outInString.joinWithSeparator(","))
+
+/* 
+this single line works too
+print(output.map({ String($0) }).joinWithSeparator(","))
+*/
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# swift-exercises
-Programming exercises in Swift
+# Programming exercises in Swift
+
+When I started learning [Swift][1], I was looking for programming challenges / problems that I can work on. I didn't find any. So I am creating this series. If you are interested, subscribe via [email](http://eepurl.com/Lu3cr).
+
+Excercises created so far:
+
+1. [Divisible by 7 but not a multiple of 5][2]: Write a program to find all numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+
+[1]: https://developer.apple.com/swift/
+[2]: http://tech.jjude.com/swift-challenge-001
